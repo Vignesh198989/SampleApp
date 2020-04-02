a0122b3269ad77c9815dc40572d5816789b38e41
Vignesh Radhakrishnan <vignesh.vicky8777@gmail.com>
2020-04-02 15:52:58 +0530

    Initial setup

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Vignesh Radhakrishnan on 02/04/20.
+//  Copyright © 2020 Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
